7aee87014294d5c6f7dc316e68aa9ba37d6dfbd1
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-10-11 14:59:40 +0200

    en ändring

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -12,59 +12,7 @@ import java.util.*;
 
 public class TestDemo {
 
-    public TestDemo() {
-
-        Scanner scan = new Scanner(System.in);
-        LocalDate t1 = LocalDate.now();
-        LocalDate ettÅrSedan = t1.minusYears(1);
-        String tempLine = "";
-        String rad2 = "";
-        //String lolo = tempLine + " " + rad2;
-        final String tempPath = "src/customers.txt";
-        final String outputPath = "src/nyaKunderLoggade.txt";
-        boolean test = false; //public boolean test = false;
-
-        double temp;
-        String svarNamn = "";
-
-        /*
-        //läser in data till programmet
-        public int readInputData(String prompt, String optionalTestParameter){
-
-            if(test){
-                scan = new Scanner(optionalTestParameter);
-            }
-            else{
-                scan = new Scanner(System.in);
-            }
-
-            while (true) {
-                try {
-                    System.out.println(prompt);
-                    return scan.nextInt();
-
-                } catch (InputMismatchException e) {
-                    System.out.println("Felaktigt tal!");
-                    scan.next();
-                } catch (NoSuchElementException e) {
-                    System.out.println("Indata saknas!");
-                    scan.next();
-                } catch (Exception e) {
-                    System.out.println("Ospecifierat fel inträffade, försök igen!");
-                    scan.next();
-                    e.printStackTrace();
-                }
-            }
-        }
-
-         */
-
-
-
-        System.out.println("Ange kundens för- och efternamn eller kundens personnummer: ");
-        svarNamn = scan.next();
-
-        /*
+    /*
     public String getCustomer(String svarNamn, String tempLine){
             if (tempLine.toLowerCase().contains(svarNamn.toLowerCase()) ) {
                 rad2.trim();
@@ -75,9 +23,26 @@ public class TestDemo {
 
          */
 
+    public TestDemo() {
+
+        Scanner scan = new Scanner(System.in);
+        LocalDate t1 = LocalDate.now();
+        LocalDate ettÅrSedan = t1.minusYears(1);
+        String kundPersonNummerOchNamn = "";
+        String kundMedlemsDatum = "";
+        final String tempPath = "src/customers.txt";
+        final String outputPath = "src/nyaKunderLoggade.txt";
+        boolean test = false; //public boolean test = false;
+        boolean personenFinns = false;
+
+        double temp;
+        String svarNamn = "";
 
 
 
+
+        System.out.println("Ange kundens för- och efternamn eller kundens personnummer: ");
+        svarNamn = scan.next();
 
 
         try {
@@ -86,41 +51,33 @@ public class TestDemo {
             PrintWriter writer = new PrintWriter(
                     new BufferedWriter(new FileWriter(outputPath, true)));
 
-            while ((tempLine = reader.readLine()) != null) {
+            while ((kundPersonNummerOchNamn = reader.readLine()) != null) {
 
-                tempLine.trim();
-                rad2 = reader.readLine();
+                kundPersonNummerOchNamn.trim();
+                kundMedlemsDatum  = reader.readLine();
 
-                //tempLine.
+                if (kundPersonNummerOchNamn.toLowerCase().contains(svarNamn.toLowerCase()) ) {
+                    kundMedlemsDatum .trim();
+                    LocalDate membership = LocalDate.parse(kundMedlemsDatum );
+                    personenFinns = true;
 
-
-                if (tempLine.toLowerCase().contains(svarNamn.toLowerCase()) ) {
-                    rad2.trim();
-                    LocalDate membership = LocalDate.parse(rad2);
-
-
-                    System.out.println("Info: " + tempLine + ", " + "Medlemskapsdatum: " + rad2);
+                    System.out.println("Info: " + kundPersonNummerOchNamn + ", "
+                            + "Medlemskapsdatum: " + kundMedlemsDatum );
                     {
                         if (membership.isAfter(ettÅrSedan)){
                             System.out.println("Du är aktiv medlem");
-                            writer.println(tempLine + " " + t1);
+                            writer.println(kundPersonNummerOchNamn + " " + t1);
                             writer.close();}
                         else
                             System.out.println(("Ditt medlemskap har utgått"));
                     }
+                    if (!personenFinns ) {
+                        System.out.println("Du har inte varit medlem förr");
+                        break;}
                 }
-                //else (!tempLine.toLowerCase().contains(svarNamn.toLowerCase()) ){
-                //System.out.println("11Du har inte varit medlem förr");
-                //break;}
-
-
-
-
-
-
-
 
             }
+
 
         } catch (Exception e) {
             System.out.println("Något gick fel");
@@ -128,17 +85,6 @@ public class TestDemo {
             System.out.flush();
             System.exit(0);
         }
-
-
-        /*
-        public boolean (LocalDate ettÅrSedan, String rad2){
-
-            return true;
-        }
-
-         */
-
-
 
 
 
